bcb9ee6f20e6f846502f0a5433235ed658463170
Alex <alexhuynh1999@gmail.com>
2022-12-24 23:31:05 -0600

    raw html for sidebar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const App = () => {
     return (
-        <h1>Hello World!</h1>
+        <div className="cotainer">
+            <div className="sidebar">
+                <a hred="#">Budget Name</a>
+                <a hred="#">Budget</a>
+                <a hred="#">Reports</a>
+                <a hred="#">All Accounts</a>
+                <a hred="#">Budget</a>
+                <a hred="#">Add Account</a>
+            </div>
+
+            <h1>Hello Alex!</h1>
+        </div>
     )
 }
 
